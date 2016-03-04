27c0df7a6bf7da11b00704c64a0a5b520fde52ae
赵新强 <zhaoxinqiang328@163.com>
2016-03-04 11:58:58 +0800

    Initial Commit

---
+++ MyPractise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPractise
+//
+//  Created by 赵新强 on 16/3/4.
+//  Copyright © 2016年 赵新强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
